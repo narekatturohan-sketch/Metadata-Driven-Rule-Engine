c8446f5e1ae082c9d21618caea2aa24621499390
narekatturohan-sketch <narekattu.rohan@gmail.com>
2026-04-21 23:02:57 +0530

    Modified from post to get

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.post("/validate/{field_code}")
+@app.get("/validate/{field_code}")
 def validate(field_code :str, value :str):
     result = validate_field(field_code, value)
     return {"field_code": field_code, "value": value, "result": result}
